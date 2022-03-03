1dc667f40317a5a2de5928801395f5488dddec69
Gabriel-Silva-Lopez <gabrieleduardosilvalopez@gmail.com>
2022-03-03 18:06:11 +0100

    primer commit

--- src/examenprog2trimestre/Ejercicio1.java
+++ src/examenprog2trimestre/Ejercicio1.java
@@ -1,5 +1,16 @@
 package examenprog2trimestre;
 
+import java.util.Scanner;
+
 public class Ejercicio1 {
+	
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		System.out.print("Dame la cantidad de jugadores que deseas: ");
+		int jugadores = entrada.nextInt();
+		System.out.print("Dame la longitud, para buscar palabras que coincidan con esa longitud: ");
+		int longitud = entrada.nextInt();
+		entrada.close();
+	}
 
 }
--- src/examenprog2trimestre/Ejercicio3.java
+++ src/examenprog2trimestre/Ejercicio3.java
@@ -2,38 +2,32 @@ package examenprog2trimestre;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Ejercicio3 {
 
-	public static void informacionVideojuegos(String nombre, int l) {
+	public static void informacionVideojuegos(String nombre) {
 		File file = new File(nombre);
 		try {
 			Scanner f = new Scanner(file);
-			int contador = 0;
-			int contadorigual = 0;
-			int contadormaslarga = 0;
 			while(f.hasNextLine()){
-				String parrafo = f.nextLine();
-				String reemplazo = parrafo.replaceAll(",","").replaceAll("\\.", "")
-						.replaceAll("\\(", "").replaceAll("\\)", "").replaceAll("\"", "")
-						.replaceAll(":", "");
-				String[] linea = reemplazo.split(" ");
-				for(int i = 0; i < linea.length; i++) {
-					if(linea[i].length() >= l) {
-						contador++;
-						if(linea[i].length() == l) {
-							contadorigual++;
-						} else {
-							contadormaslarga++;
+				ArrayList<String> plataformas = new ArrayList<String>();
+				String linea = f.nextLine();
+				String[] separador = linea.split(",");
+				boolean empiezo = false;
+				if(empiezo) {
+					for(int i = 0; i < linea.length(); i++) {
+						if(separador[i].contains("\"")) {
+							separador[i] = separador[i] + separador[i+1];
+							System.out.println(separador[i]);
 						}
+					}				
+				} else {
+						empiezo = true;
 					}
-
 				}
-			}
-			System.out.println("\nLas palabras que coinciden o sobrepasan esa la longitud son: " + contador);
-			System.out.println("Iguales a la longitud: " + contadorigual);
-			System.out.println("Mas largas que la longitud: " + contadormaslarga);
+
 			f.close();
 		} catch(FileNotFoundException e) {
 			System.out.println("El fichero " + nombre + " no ha podido ser abierto.");
@@ -44,9 +38,7 @@ public class Ejercicio3 {
 		Scanner entrada = new Scanner(System.in);
 		System.out.print("Dame la ruta + nombre del fichero: ");
 		String nombreFichero = entrada.nextLine();
-		System.out.print("Dame la longitud, para buscar palabras que coincidan con esa longitud: ");
-		int longitud = entrada.nextInt();
-		palabrasMasLargasQue(nombreFichero, longitud);
+		informacionVideojuegos(nombreFichero);
 		entrada.close();
 	}
 }
